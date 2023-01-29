e69cbb83072da854a1eacbd99d25618832408361
keyminy <imanaifs@gmail.com>
2023-01-30 01:31:06 +0900

    7장 끝,게시물 첨부파일 수정&삭제

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -1,7 +1,14 @@
 package org.zerock.b01.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,7 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.BoardDTO;
-import org.zerock.b01.dto.BoardListReplyCountDTO;
+import org.zerock.b01.dto.BoardListAllDTO;
 import org.zerock.b01.dto.PageRequestDTO;
 import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.service.BoardService;
@@ -26,12 +33,15 @@ public class BoardController {
 	
 	private final BoardService boardService;
 	
+	@Value("${org.zerock.upload.path}")
+	private String uploadPath;
+	
 	@GetMapping("/list")
 	public void list(PageRequestDTO pageRequestDTO,Model model) {
 		//PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
 		
-		PageResponseDTO<BoardListReplyCountDTO> responseDTO
-				= boardService.listWithReplyCount(pageRequestDTO);
+		PageResponseDTO<BoardListAllDTO> responseDTO
+				= boardService.listWithAll(pageRequestDTO);
 		log.info(responseDTO);
 		model.addAttribute("responseDTO",responseDTO);
 	}
@@ -90,10 +100,38 @@ public class BoardController {
 	}
 	
 	@PostMapping("/remove")
-	public String remove(Long bno,RedirectAttributes redirectAttributes) {
-		log.info("remove post.." + bno);
-		boardService.remove(bno);
+	public String remove(BoardDTO boardDTO,RedirectAttributes redirectAttributes) {
+		log.info("remove post.." + boardDTO.getBno());
+		boardService.remove(boardDTO.getBno());
+		
+		/* 게시물이, 데이터베이스 상에서 삭제되었다면 첨부파일도 삭제 */
+		log.info(boardDTO.getFileNames());
+		List<String> fileNames = boardDTO.getFileNames();
+		if(fileNames != null && fileNames.size()>0) {
+			removeFiles(fileNames);
+		}
+		
 		redirectAttributes.addFlashAttribute("result","removed");
 		return "redirect:/board/list";
+	}
+	
+	public void removeFiles(List<String> files) {
+		for(String fileName : files) {
+			Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+			String resourceName = resource.getFilename();
+			
+			try {
+				String contentType = Files.probeContentType(resource.getFile().toPath());
+				resource.getFile().delete();
+				//썸네일이 존재한다면?
+				if(contentType.startsWith("image")) {
+					File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+					thumbnailFile.delete();
+				}
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}
+			
+		}//end for
 	}
 }
